0c6468d2362c97c4be9d14552a45ac14a0e93632
Andres <andresitomanuel@gmail.com>
2024-03-12 15:08:29 -0300

    Add project files.

---
+++ Excel.Library/Attributes/ExcelAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Excel.Library.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+public class ExcelAttribute : Attribute
+{
+    public ExcelAttribute(string? Name = null, bool? IsProperty = null, object? defaultValue = null)
+    {
+        this.Name = Name;
+        this.IsProperty = IsProperty;
+        DefaultValue = defaultValue;
+    }
+
+    public string? Name { get; set; }
+    public bool? IsProperty { get; set; }
+    public object? DefaultValue { get; set; }
+}
